a959525a4cd0c6aedb473b88ed02f540bd194d74
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-11-08 18:42:26 +0600

    implemented json-server

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -8,7 +9,7 @@ const {
 } = require('graphql')
 
 // Hardcoded Data
-const customers = [
+/* const customers = [
   {
     id: '1',
     name: 'Shahadat Hossain',
@@ -23,7 +24,7 @@ const customers = [
     age: 70
   },
   { id: '4', name: 'S M Sultan', email: 'sm.sultan@gmail.com', age: 60 }
-]
+]  */
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -46,17 +47,21 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve(parentValue, args) {
-        for (let i = 0; i < customers.length; i++) {
+        /* for (let i = 0; i < customers.length; i++) {
           if (customers[i].id == args.id) {
             return customers[i]
           }
-        }
+        } */
+
+        return axios
+          .get('http:localhost:3000/customers/' + args.id)
+          .then(res => res.data)
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args) {
-        return customers
+        return axios.get('http:localhost:3000/customers').then(res => res.data)
       }
     }
   }
