831b6e30c6a08875f3d2bf19f1d1f1cdfaac2b9f
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-11-08 18:20:23 +0600

    fetch customers

--- schema.js
+++ schema.js
@@ -52,6 +52,12 @@ const RootQuery = new GraphQLObjectType({
           }
         }
       }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args) {
+        return customers
+      }
     }
   }
 })
